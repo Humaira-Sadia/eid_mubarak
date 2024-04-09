899d0ca7c8f6c8aa61e512c5b6abaef9945bb5d0
Humaira Sadia <humaira.sadia.hossain@gmail.com>
2024-04-09 23:40:41 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from "react";
+import { chatData } from "./constants.js";
+
+const App = () => {
+  const [displayedChatData, setDisplayedChatData] = useState([]);
+  const [initialMessageDisplayed, setInitialMessageDisplayed] = useState(false);
+
+  useEffect(() => {
+    const displayMessagesWithDelay = () => {
+      let delay = 0;
+      const displayedMessages = [];
+      chatData.forEach((item, index) => {
+        delay += 3000; 
+        setTimeout(() => {
+          displayedMessages.unshift(item);
+          setDisplayedChatData([...displayedMessages]);
+        }, delay);
+      });
+      
+      setTimeout(() => {
+        setInitialMessageDisplayed(true);
+      }, 5000); 
+    };
+
+    displayMessagesWithDelay();
+  }, []);
+
+  return (
+    <div className="sm:hidden font-ubuntu h-screen flex flex-col">
+      <div className="chatItem flex flex-col-reverse items-end overflow-y-auto">
+        {displayedChatData.map((item, index) => (
+          <div
+            key={index}
+            className={
+              item.sentByMe
+                ? "me p-2 m-4 border rounded-3xl me_grad sm:w-1/2 w-2/3 self-end"
+                : "other p-2 m-4 border rounded-3xl other_grad sm:w-1/2 w-2/3 self-start"
+            }
+          >
+            <p>{item.mesg}</p>
+          </div>
+        ))}
+      </div>
+      {initialMessageDisplayed && displayedChatData.length > 0 && displayedChatData[0].sentByMe && (
+        <div className="mesg_field w-16 mt-auto mb-3 mx-4 p-3 border rounded-3xl flex justify-evenly">
+          <div className="circle w-2 h-2 bg-white rounded-3xl"></div>
+          <div className="circle w-2 h-2 bg-white rounded-3xl"></div>
+          <div className="circle w-2 h-2 bg-white rounded-3xl"></div>
+        </div>
+      )}
+      {displayedChatData.length === chatData.length && (
+        <DelayedVideo src="eid_mubarak.mp4" delay={3000} autoPlay muted />
+      )}
+    </div>
+  );
+};
+
+const DelayedVideo = ({ src, delay, ...props }) => {
+  const [showVideo, setShowVideo] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowVideo(true);
+    }, delay);
+
+    return () => clearTimeout(timer);
+  }, [delay]);
+
+  return showVideo ? <video src={src} {...props}></video> : null;
+};
+
+export default App;
---
+++ src/constants.js
@@ -0,0 +1,40 @@
+const chatData = [
+    {
+        id: 1,
+        dp: "#",
+        mesg: "Assalam Alaikum, Eid Mubarak 😄",
+        sentByMe: true
+    },
+    {
+        id: 2,
+        dp: "#",
+        mesg: "Walaekum Assalam, Khair Mubarak 🥰",
+        sentByMe: false
+    },
+    {
+        id: 3,
+        dp: "#",
+        mesg: "May this Eid bring you joy that fills your heart with warmth, laughter that echoes through your home, and blessings that light up your path. Wishing you and your loved ones a beautiful and heartwarming Eid filled with love, peace, and endless happiness. Eid Mubarak 🌙✨",
+        sentByMe: true
+    },
+    {
+        id: 4,
+        dp: "#",
+        mesg: "Eid Mubarak! Warm wishes for joy, laughter, blessings, and togetherness😄😄",
+        sentByMe: false
+    },
+    {
+        id: 5,
+        dp: "#",
+        mesg: "I have something else, wanna see more??",
+        sentByMe: true
+    },
+    {
+        id: 6,
+        dp: "#",
+        mesg: "Yeah sure😻😻",
+        sentByMe: false
+    }
+];
+
+export  {chatData};
